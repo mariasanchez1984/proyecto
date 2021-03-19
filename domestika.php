e67439f3150b63da2415bfcf9cb924e58f60a2c4
María Sánchez Vielba <70698506+mariasanchez1984@users.noreply.github.com>
2021-03-19 20:42:32 +0100

    c

--- domestika.php
+++ domestika.php
@@ -27,10 +27,10 @@
     </header>
     <div class="col-5 col-md-4 col-sm-3 col-xs-6 contacto-titulo">
   <h1>-Domestika-</h1>
+  <a href="https://www.domestika.org/es"></a>
 </div>
 <h3>Breve descripción de la empresa</h3>
     <p>Inspírate con profesionales destacados del sector aprendiendo sus técnicas y secretos. Aprende en comunidad, compartiendo ideas y proyectos con miles de creativos del mundo. Desde Todos Los Niveles</p>
-    <a href="https://www.domestika.org/es"></a>
 <a href="index.php" id="backButton"><i class="far fa-arrow-alt-circle-left site-nav--icon"></i></a>
 <div class="footer">
         &copy; All rights reserved. <br>Diseño de: María S Vielba
